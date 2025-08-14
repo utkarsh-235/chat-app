70b448f018d4602c4b5e07995622d92612744ae2
Utkarsh Agarwal <agarwalutkarsh1123@gmail.com>
2025-08-15 03:15:04 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-git-main-utkarshs-projects-7cc34808.vercel.app",
     credentials: true,
   })
 );
